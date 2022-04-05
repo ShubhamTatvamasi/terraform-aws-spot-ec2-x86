d6f7ad59a3adc67812ae360876dbd97dbc6c2dff
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2022-04-05 11:06:20 +0530

    modified:   main.tf

--- interface.tf
+++ interface.tf
@@ -1,6 +1,15 @@
-# resource "aws_network_interface" "second_interface" {
-#   subnet_id       = local.subnet_id
-#   security_groups = [
-#     module.local_vpc_sg.security_group_id
-#   ]
-# }
+resource "aws_network_interface" "second_interface" {
+
+  subnet_id       = local.subnet_id
+
+  security_groups = [
+    module.local_vpc_sg.security_group_id
+  ]
+
+  attachment {
+    instance     = module.ec2_instance.spot_instance_id
+    device_index = 1
+  }
+
+  tags = local.tags
+}
