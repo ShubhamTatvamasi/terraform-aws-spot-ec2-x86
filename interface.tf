bf2e96bb8cd359c8de4ac2668f48b1318b703cd9
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2022-04-05 11:32:00 +0530

    modified:   interface.tf

--- interface.tf
+++ interface.tf
@@ -1,7 +1,7 @@
 resource "aws_network_interface" "second_interface" {
   count = 1
 
-  subnet_id       = local.subnet_id
+  subnet_id = local.subnet_id
 
   security_groups = [
     module.local_vpc_sg.security_group_id
--- local.tf
+++ local.tf
@@ -1,9 +1,9 @@
 locals {
   region = "us-west-1"
 
-  subnet_id = "subnet-2674e740"
+  subnet_id     = "subnet-2674e740"
   instance_type = "t3a.medium"
-  volume_size = 50
+  volume_size   = 50
 
   ami = "ami-075fd582acf0c0128" # Linux kernal 5.4.0-1009-aws - Ubuntu 20.04 LTS - us-west-1
 
